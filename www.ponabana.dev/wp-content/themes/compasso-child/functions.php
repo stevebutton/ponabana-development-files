5eeb5e4c69c928e4fa6ccc7a5d8c385f7b05eea9
stevebutton <stevebutton@me.com>
2014-05-26 19:28:43 +0200

    try featured link to post

--- www.ponabana.dev/wp-content/themes/compasso-child/functions.php
+++ www.ponabana.dev/wp-content/themes/compasso-child/functions.php
@@ -19,6 +19,15 @@ add_filter('get_the_excerpt', 'custom_trim_excerpt');
 
 remove_filter('template_redirect', 'redirect_canonical');
 
+add_filter( 'post_thumbnail_html', 'my_post_image_html', 10, 3 );
+
+function my_post_image_html( $html, $post_id, $post_image_id ) {
+
+  $html = '<a href="' . get_permalink( $post_id ) . '" title="' . esc_attr( get_post_field( 'post_title', $post_id ) ) . '">' . $html . '</a>';
+  return $html;
+
+}
+
 function custom_trim_excerpt($text) { // Fakes an excerpt if needed
 global $post;
 if ( '' == $text ) {
