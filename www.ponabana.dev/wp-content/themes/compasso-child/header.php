df5631a80ae9fa9e480cf9f5ffe0f8980f2aae37
stevebutton <stevebutton@me.com>
2014-11-17 16:18:46 +0100

    countdown widget

--- www.ponabana.dev/wp-content/themes/compasso-child/header.php
+++ www.ponabana.dev/wp-content/themes/compasso-child/header.php
@@ -88,11 +88,11 @@
 			</nav>
  <div id="callout">
  <?php if(ICL_LANGUAGE_CODE=='fr'): ?>
- <a href="http://ponabana.com/category/crc/" target="_self"><img src="/gfx/crcfr.png" alt=""></a>
+ <a href="http://ponabana.com/category/crc/" target="_self"><img src="/gfx/crcfr.png" alt="" width="632" height="100"></a>
 <?php elseif(ICL_LANGUAGE_CODE=='en'): ?>
-<a href="http://ponabana.com/category/crc-en/?lang=en" target="_self"><img src="/gfx/crcen.png" alt=""></a>
+<a href="http://ponabana.com/category/crc-en/?lang=en" target="_self"><img src="/gfx/crcen.png" alt="" width="632" height="100"></a>
 <?php endif;?>
-<div id="countdown"><?php echo do_shortcode('[tminus t="20-11-2014 12:12:12"]'); ?></div></div><div id="unieleze">
+<div id="countdown"><?php echo do_shortcode('[tminus t="20-11-2014 12:12:12"]'); ?></div></div>
  <div id="unieleze"><?php if(ICL_LANGUAGE_CODE=='fr'): ?>
    <a href="http://unieleze.cd/" target="new"><img src="/gfx/unilezegrabfr.png"></a>
 <?php elseif(ICL_LANGUAGE_CODE=='en'): ?>
